d1ebb9429508d24b9ea30c420cb878c549747f3d
desmandemaker <144113791+desmandemaker@users.noreply.github.com>
2023-10-04 11:15:35 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 console.log("hi");
 
 const hamburger = document.querySelector("header nav button");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector("header nav ul");
 
 
 
@@ -12,7 +12,7 @@ hamburger.addEventListener("click", () =>{
 
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n. 
+document.querySelectorAll("header nav ul li a").forEach(n => n. 
     addEventListener("click", ()=> {
         hamburger.classList.remove("active")
         navMenu.classList.remove("active")
@@ -67,3 +67,29 @@ document.querySelectorAll(".nav-link").forEach(n => n.
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Voeg een eventlistener toe om de dropdown te activeren bij klikken op de knop
+    document.querySelectorAll('footer ul li button').forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            e.stopPropagation(); // Stop de gebeurtenispropagatie om te voorkomen dat de pagina sluit
+            const parentLi = this.parentElement;
+            parentLi.classList.toggle('active');
+            
+            // Sluit alle andere dropdowns
+            document.querySelectorAll('footer ul li').forEach(function (dropdown) {
+                if (dropdown !== parentLi) {
+                    dropdown.classList.remove('active');
+                }
+            });
+        });
+    });
+
+    // Sluit de dropdown als er ergens anders op de pagina wordt geklikt
+    document.addEventListener('click', function (event) {
+        document.querySelectorAll('footer ul li').forEach(function (dropdown) {
+            if (!dropdown.contains(event.target)) {
+                dropdown.classList.remove('active');
+            }
+        });
+    });
+});
